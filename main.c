81b9a0dfeba2a323c363e11e69a4911c232b18d1
Youssef <yooussef190@gmail.com>
2025-12-06 18:58:22 +0200

    read a move ,input format: A7A8Q OR A7 A8Q,no space between 8 ,Q

--- main.c
+++ main.c
@@ -24,10 +24,11 @@ void showBoard(char b[8][8]){
     }
     printf("   ");
     for(int i=0;i<8;i++){
-        int k='a';
+        int k='A';
         k=k+i;
         printf("%c ",k);
     }
+    printf("\n");
 }
 void move(char B[8][8], int row1, int column1, int row2, int column2)
 {
@@ -39,14 +40,40 @@ void move(char B[8][8], int row1, int column1, int row2, int column2)
                                   later add logic to save the captured piece to a list  */
     B[row1][column1] = (row1 + column1) % 2 == 0 ? '-' : '.'; 
 }
+void readmove(int *r1,int *c1,int *r2,int *c2,char *prom){
+    char from[3],to[4];
+    int ch;
+    while(1){
+        scanf("%2s %3s",from,to);
+        while ((ch = getchar()) != '\n' && ch != EOF);  // clean buffer
+        *r1='8'-from[1];
+        *c1=from[0]-'A';
+        *r2='8'-to[1];
+        *c2=to[0]-'A';
+        *prom=(to[2]=='\0')?'\0':to[2];
+        if(*r1 >=0 && *r1 <=8 && *r2 >=0 && *r2 <=8 && *c1 >=0 && *c1 <=8 && *c2 >=0 && *c2 <=8\
+        && (*prom=='\0' ||*prom=='B' ||*prom=='N' ||*prom=='Q' ||*prom=='R')){
+            break;
+        }
+        else
+        printf("Invalid move, try again!\n");
+}
+
+    
+
+}
 void main(){
+    int row1,col1,row2,col2;
+    char prom;
     char turn=0;
     char board[8][8];
     Board(board);
   //  while(1){
         char *a=turn==0?"White":"Black";
-        printf("%s's Turn\n",a);
         showBoard(board);
+        printf("%s's Turn\n",a);
+        readmove(&row1,&col1,&row2,&col2,&prom);
         turn=1-turn;
+        printf("%d %d %d %d %c",row1,col1,row2,col2,prom);
   //  }
 }
